80dc0aa2abfc2a0bd483732312da2684a7ccd714
Shivam Kumar <shivamkajla32@gmail.com>
2026-05-17 14:50:24 +0530

    fixed auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,13 +2,15 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
-      return res.json({
-        message: "No token",
+    if (!authHeader) {
+      return res.status(401).json({
+        message: "No token provided",
       });
     }
+
+    const token = authHeader.split(" ")[1];
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
@@ -17,6 +19,10 @@ const authMiddleware = (req, res, next) => {
     next();
   } catch (error) {
     console.log(error);
+
+    res.status(401).json({
+      message: "Invalid token",
+    });
   }
 };
 
